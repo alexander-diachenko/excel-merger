f70add20cb493c3c26534043a347015aef360dcd
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-13 15:55:01 +0300

    правильно считывает и записывает файл

---
+++ src/main/java/ReadWriteExcel.java
@@ -0,0 +1,67 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User on 13.06.2017.
+ */
+public class ReadWriteExcel {
+    private static final String FILE_NAME = "D:\\Downloads\\test.xlsx";
+
+    public static void main(String[] args) {
+        List<List<Object>> table = new ArrayList<>();
+        FileInputStream excelFile;
+        Workbook workbook = new XSSFWorkbook();
+        try {
+            excelFile = new FileInputStream(new File(FILE_NAME));
+            workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+
+            for (Row aDatatypeSheet : datatypeSheet) {
+                List<Object> row = new ArrayList<>();
+                for (Cell currentCell : aDatatypeSheet) {
+                    //getCellTypeEnum shown as deprecated for version 3.15
+                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+                    Object cell = new Object();
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        cell = currentCell.getStringCellValue();
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                        cell = currentCell.getNumericCellValue();
+                    }
+                    row.add(cell);
+                }
+                table.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Sheet sheet = workbook.createSheet("Datatypes in Java");
+        int rowNum = 0;
+        for (int rowIndex = 0; rowIndex < table.size(); rowIndex++) {
+            List<Object> rows = table.get(rowIndex);
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (int cellIndex = 0; cellIndex < rows.size(); cellIndex++) {
+                Object cell1 = rows.get(cellIndex);
+                Cell cell = row.createCell(colNum++);
+                if (cell1 instanceof String) {
+                    cell.setCellValue((String) cell1);
+                } else if (cell1 instanceof Integer) {
+                    cell.setCellValue((Integer) cell1);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream("D:\\Downloads\\updated.xlsx");
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
