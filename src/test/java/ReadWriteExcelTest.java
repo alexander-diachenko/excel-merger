3c722587503f04a6c97bc2b7f3c039784392a61a
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-20 16:58:49 +0300

    написал тест для записи файла с одним полем

--- src/test/java/ReadWriteExcelTest.java
+++ src/test/java/ReadWriteExcelTest.java
@@ -2,6 +2,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,6 +26,19 @@ public class ReadWriteExcelTest {
         Assert.assertEquals(
                 "[[SAB070001, ANNA SUI ROMANTICA EDT TESTER 75 ml spray  с крышкoй, 10], " +
                         "[SAB070002, ANNA SUI ROMANTICA EDT 30 ml spray, 42]]", table.toString());
+    }
+
+    @Test
+    public void writeExcelTest_oneField() throws IOException {
+        ReadWriteExcel readWriteExcel = new ReadWriteExcelImpl();
+        List<List<Object>> table = new ArrayList<>();
+        List<Object> raw = Arrays.asList("SBA160002", "8411061784273", "ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray", "100", "EDT", "М", "15,30");
+        table.add(raw);
+        File got = File.createTempFile("got-", ".xlsx");
+        String outputPath = got.getAbsolutePath();
+        readWriteExcel.write(table, outputPath);
+        List<List<Object>> tempTable = readWriteExcel.read(outputPath);
+        Assert.assertEquals("[[SBA160002, 8411061784273, ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray, 100, EDT, М, 15,30]]", tempTable.toString());
     }
 
     private String getFilePath(String fileName) {
