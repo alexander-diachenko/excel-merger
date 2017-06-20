a34e6629fc16cf264cb5d62a89ccb1d1d4d59f70
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-20 16:14:58 +0300

    добавил тест на считку 2х полей

--- src/test/java/ReadWriteExcelTest.java
+++ src/test/java/ReadWriteExcelTest.java
@@ -10,10 +10,19 @@ import java.util.List;
 public class ReadWriteExcelTest {
 
     @Test
-    public void readExcelTest_OneField() {
+    public void readExcelTest_oneField() {
         ReadWriteExcel readWriteExcel = new ReadWriteExcelImpl();
         List<List<Object>> table = readWriteExcel.read(getFilePath("file/oneField.xlsx"));
         Assert.assertEquals("[[SAG060003, AGENT PROVOCATEUR FATALE EDP 50 ml spray, 6, 3760264453741]]", table.toString());
+    }
+
+    @Test
+    public void readExcelTest_twoField() {
+        ReadWriteExcel readWriteExcel = new ReadWriteExcelImpl();
+        List<List<Object>> table = readWriteExcel.read(getFilePath("file/twoField.xlsx"));
+        Assert.assertEquals(
+                "[[SAB070001, ANNA SUI ROMANTICA EDT TESTER 75 ml spray  с крышкoй, 10], " +
+                        "[SAB070002, ANNA SUI ROMANTICA EDT 30 ml spray, 42]]", table.toString());
     }
 
     private String getFilePath(String fileName) {
