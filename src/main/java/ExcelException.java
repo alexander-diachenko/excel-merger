c5c2dd9f6ecda83465749a2f986319ee5302a592
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-26 14:53:09 +0300

    добавил свой ексепшн

---
+++ src/main/java/ExcelException.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 26.06.2017.
+ */
+public class ExcelException extends Exception{
+
+    public ExcelException(String message, Exception e){
+        super(message, e);
+    }
+}
