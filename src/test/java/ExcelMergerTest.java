0cee4a1770b9da43552bb22159fc3f1ea70009d1
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-27 16:19:17 +0300

    и написал тест чтобы закрепить этот кейс :)

--- src/test/java/ExcelMergerTest.java
+++ src/test/java/ExcelMergerTest.java
@@ -99,4 +99,23 @@ public class ExcelMergerTest {
         final List<List<Object>> tableMerged = excelMerger.mergeOneField(articles, fields);
         Assert.assertEquals("[[SAN030003, 1234567891234]]", tableMerged.toString());
     }
+
+    @Test
+    public void mergeTest_oneField_From_ColumnNull() {
+        final List<List<Object>> tableTo = new ArrayList<>();
+        List<Object> rowTo = new ArrayList<>();
+        rowTo.add("SAN030003");
+        tableTo.add(rowTo);
+
+        final List<List<Object>> tableFrom = new ArrayList<>();
+        final List<Object> rowFrom = new ArrayList<>();
+        rowFrom.add("SAN030003");
+        tableFrom.add(rowFrom);
+
+        final List<Integer> articles = Arrays.asList(0, 0);
+        final List<Integer> fields = Arrays.asList(1, 1);
+        final ExcelMerger excelMerger = new ExcelMergerImpl(tableFrom, tableTo);
+        final List<List<Object>> tableMerged = excelMerger.mergeOneField(articles, fields);
+        Assert.assertEquals("[[SAN030003]]", tableMerged.toString());
+    }
 }
