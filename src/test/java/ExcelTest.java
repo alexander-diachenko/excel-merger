114efeae514ec52af267eaf075434a8f6a5dfd17
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-29 13:59:43 +0300

    Добавил файнелы

--- src/test/java/ExcelTest.java
+++ src/test/java/ExcelTest.java
@@ -16,23 +16,23 @@ import java.util.List;
  */
 public class ExcelTest {
 
-    private Excel excel = new ExcelImpl();
+    private final Excel excel = new ExcelImpl();
 
     @Test
     public void readExcelTest_oneField() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/oneField.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/oneField.xlsx"));
         Assert.assertEquals("[[SAG060003, AGENT PROVOCATEUR FATALE EDP 50 ml spray, 6, 3760264453741]]", table.toString());
     }
 
     @Test
     public void readExcelTest_oneField_Xls() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/oneField.xls"));
+        final List<List<Object>> table = excel.read(getFilePath("file/oneField.xls"));
         Assert.assertEquals("[[SAZ010009, AZZARO CHROME EDT TESTER 100 ml spray]]", table.toString());
     }
 
     @Test
     public void readExcelTest_twoField() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/twoField.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/twoField.xlsx"));
         Assert.assertEquals(
                 "[[SAB070001, ANNA SUI ROMANTICA EDT TESTER 75 ml spray  с крышкoй, 10], " +
                         "[SAB070002, ANNA SUI ROMANTICA EDT 30 ml spray, 42]]", table.toString());
@@ -40,34 +40,34 @@ public class ExcelTest {
 
     @Test
     public void readExcelTest_oneField_withSpace() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/oneFieldWithSpace.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/oneFieldWithSpace.xlsx"));
         Assert.assertEquals(
                 "[[SBA030010, , 34]]", table.toString());
     }
 
     @Test
     public void writeExcelTest_oneField() throws IOException, InvalidFormatException {
-        List<List<Object>> table = new ArrayList<>();
-        List<Object> raw = Arrays.asList("SBA160002", "8411061784273", "ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray", "100", "EDT", "М", "15,30");
+        final List<List<Object>> table = new ArrayList<>();
+        final List<Object> raw = Arrays.asList("SBA160002", "8411061784273", "ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray", "100", "EDT", "М", "15,30");
         table.add(raw);
-        File got = File.createTempFile("got-", ".xlsx");
-        String outputPath = got.getAbsolutePath();
+        final File got = File.createTempFile("got-", ".xlsx");
+        final String outputPath = got.getAbsolutePath();
         excel.write(table, outputPath);
-        List<List<Object>> tempTable = excel.read(outputPath);
+        final List<List<Object>> tempTable = excel.read(outputPath);
         Assert.assertEquals("[[SBA160002, 8411061784273, ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray, 100, EDT, М, 15,30]]", tempTable.toString());
     }
 
     @Test
     public void writeExcelTest_twoField() throws IOException, InvalidFormatException {
-        List<List<Object>> table = new ArrayList<>();
-        List<Object> raw1 = Arrays.asList("SBA160002", "8411061784273", "ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray", "100", "EDT", "М", "15,30");
-        List<Object> raw2 = Arrays.asList("SAN020002", "8427395660206", "ANGEL SCHLESSER HOMME EDT 125 ml spray", "125", "EDT", "М", "16,40");
+        final List<List<Object>> table = new ArrayList<>();
+        final List<Object> raw1 = Arrays.asList("SBA160002", "8411061784273", "ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray", "100", "EDT", "М", "15,30");
+        final List<Object> raw2 = Arrays.asList("SAN020002", "8427395660206", "ANGEL SCHLESSER HOMME EDT 125 ml spray", "125", "EDT", "М", "16,40");
         table.add(raw1);
         table.add(raw2);
-        File got = File.createTempFile("got-", ".xlsx");
-        String outputPath = got.getAbsolutePath();
+        final File got = File.createTempFile("got-", ".xlsx");
+        final String outputPath = got.getAbsolutePath();
         excel.write(table, outputPath);
-        List<List<Object>> tempTable = excel.read(outputPath);
+        final List<List<Object>> tempTable = excel.read(outputPath);
         Assert.assertEquals(
                 "[[SBA160002, 8411061784273, ANTONIO BANDERAS KING OF SEDUCTION  MAN EDT 100 ml spray, 100, EDT, М, 15,30], " +
                         "[SAN020002, 8427395660206, ANGEL SCHLESSER HOMME EDT 125 ml spray, 125, EDT, М, 16,40]]", tempTable.toString());
@@ -75,7 +75,7 @@ public class ExcelTest {
 
     @Test
     public void readExcelTest_twoField_different_size() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/twoFieldDifferentSizeFirstShorter.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/twoFieldDifferentSizeFirstShorter.xlsx"));
         Assert.assertEquals(
                 "[[SOT440001, 3760260453042], " +
                         "[SOT190003, 3760260451994, 50 ml, U]]", table.toString());
@@ -83,14 +83,14 @@ public class ExcelTest {
 
     @Test
     public void readExcelTest_oneField_withSpaces_beginningAndMiddle() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/oneFieldSpacesAtBeginningAndAtMiddle.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/oneFieldSpacesAtBeginningAndAtMiddle.xlsx"));
         Assert.assertEquals(
                 "[[, , SOT440001, , , 3760260453042]]", table.toString());
     }
 
     @Test
     public void readExcelTest_twoField_different_size2() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/twoFieldDifferentSizeSecondShorter.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/twoFieldDifferentSizeSecondShorter.xlsx"));
         Assert.assertEquals(
                 "[[SOT190003, 3760260451994, 50 ml, U], " +
                         "[SOT440001, 3760260453042]]", table.toString());
@@ -98,7 +98,7 @@ public class ExcelTest {
 
     @Test
     public void readExcelTest_threeField_different_size() throws IOException, InvalidFormatException {
-        List<List<Object>> table = excel.read(getFilePath("file/threeFieldDifferentSize.xlsx"));
+        final List<List<Object>> table = excel.read(getFilePath("file/threeFieldDifferentSize.xlsx"));
         Assert.assertEquals(
                 "[[SOT190003, 3760260451994, 50 ml, U], " +
                         "[SOT440001, 3760260453042], " +
@@ -106,7 +106,7 @@ public class ExcelTest {
     }
 
     private String getFilePath(String fileName) {
-        ClassLoader classLoader = getClass().getClassLoader();
+        final ClassLoader classLoader = getClass().getClassLoader();
         final URL resource = classLoader.getResource(fileName);
         if (resource != null) {
             return new File(resource.getFile()).getAbsolutePath();
