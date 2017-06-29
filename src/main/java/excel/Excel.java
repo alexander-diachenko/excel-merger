7fe8ee31a4dd18c15cffdda5ac5c2c13fb360e66
alwxander.diachenko <mail.positiv@gmail.com>
2017-06-29 14:15:12 +0300

    написал документацию к интерфейсам

--- src/main/java/excel/Excel.java
+++ src/main/java/excel/Excel.java
@@ -9,7 +9,20 @@ import java.util.List;
  * @author Alexander Diachenko.
  */
 public interface Excel {
+    /**
+     * Return list representation of excel file.
+     * @param path Path to excel file.
+     * @return List representation of excel file.
+     * @throws IOException Throws IOException if file read failed.
+     * @throws InvalidFormatException Throws InvalidFormatException if it is not excel file(.xls or .xlsx).
+     */
     List<List<Object>> read(final String path) throws IOException, InvalidFormatException;
 
+    /**
+     * Write List<List<>> to excel file.
+     * @param table Data in List<List<>>.
+     * @param path Path to new excel file.
+     * @throws IOException Throws IOException if file write failed.
+     */
     void write(final List<List<Object>> table, final String path) throws IOException;
 }
--- src/main/java/excel/ExcelMerger.java
+++ src/main/java/excel/ExcelMerger.java
@@ -6,5 +6,11 @@ import java.util.List;
  * @author Alexander Diachenko.
  */
 public interface ExcelMerger {
+    /**
+     * Return merged lists.
+     * @param articles List<Integer>. First index number of id column from file. Second index number of id column to file.
+     * @param fields List<Integer>.  First index number of merged column from file. Second index number of merged column to file.
+     * @return Merged lists.
+     */
     List<List<Object>> mergeOneField(final List<Integer> articles, final List<Integer> fields);
 }
