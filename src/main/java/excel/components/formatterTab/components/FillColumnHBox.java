f1c3cccb0148aa96eee02f5b555aa883931eef05
alwxander.diachenko <mail.positiv@gmail.com>
2017-09-06 09:41:08 +0300

    добавил чекбокс и текстфилды для заполнения колонки

--- src/main/java/excel/components/formatterTab/components/FillColumnHBox.java
+++ src/main/java/excel/components/formatterTab/components/FillColumnHBox.java
@@ -1,5 +1,8 @@
 package excel.components.formatterTab.components;
 
+import excel.Util.RegexUtil;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 /**
@@ -7,7 +10,43 @@ import javafx.scene.layout.HBox;
  */
 public class FillColumnHBox extends HBox {
 
-    public FillColumnHBox() {
+    private final TextField columnNumber;
+    private final TextField fill;
 
+    public FillColumnHBox() {
+        final CheckBox isFill = new CheckBox();
+        isFill.setSelected(false);
+
+        columnNumber = new TextField();
+        columnNumber.setPromptText("Enter column number");
+        columnNumber.setDisable(true);
+        columnNumber.setFocusTraversable(true);
+        columnNumber.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches(RegexUtil.getNumericRegex())) {
+                columnNumber.setText(oldValue);
+            }
+        });
+
+        fill = new TextField();
+        fill.setPromptText("Enter what to fill");
+        fill.setDisable(true);
+        fill.setFocusTraversable(true);
+
+        isFill.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            columnNumber.setText("");
+            fill.setText("");
+            columnNumber.setDisable(!newValue);
+            fill.setDisable(!newValue);
+        });
+
+        getChildren().addAll(isFill, columnNumber, fill);
+    }
+
+    public TextField getColumnNumber() {
+        return columnNumber;
+    }
+
+    public TextField getFill() {
+        return fill;
     }
 }
