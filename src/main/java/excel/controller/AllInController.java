b2d7021f1dd674f8358bcc28e864be9afbf1223c
alwxander.diachenko <mail.positiv@gmail.com>
2018-03-23 17:30:04 +0200

    сделал методы для выбора файло и папки для сохранение в контроллере ол-ина

--- src/main/java/excel/controller/AllInController.java
+++ src/main/java/excel/controller/AllInController.java
@@ -1,9 +1,17 @@
 package excel.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * @author Alexander Diachenko.
@@ -11,7 +19,9 @@ import javafx.scene.control.ProgressIndicator;
 public class AllInController {
 
     @FXML
-    private Button file;
+    private AnchorPane anchorPane;
+    @FXML
+    private Button files;
     @FXML
     private Button saveDirectory;
     @FXML
@@ -19,11 +29,39 @@ public class AllInController {
     @FXML
     private Button open;
     @FXML
-    private Label filePath;
+    private Label filesCount;
     @FXML
     private Label saveDirectoryPath;
     @FXML
     private Label complete;
     @FXML
     private ProgressIndicator progressIndicator;
+
+    public void selectFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        List<File> files = fileChooser.showOpenMultipleDialog(getStage());
+        if(files != null) {
+            filesCount.setText(files.size() + " file(s)");
+        } else {
+            filesCount.setText("");
+        }
+    }
+
+    public void selectSaveDirectory(ActionEvent actionEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File file = directoryChooser.showDialog(getStage());
+        if (file != null) {
+            saveDirectoryPath.setText(file.getAbsolutePath());
+        } else {
+            saveDirectoryPath.setText("");
+        }
+    }
+
+    private Stage getStage() {
+        return (Stage) anchorPane.getScene().getWindow();
+    }
+
+    public void allIn(ActionEvent actionEvent) {
+
+    }
 }
