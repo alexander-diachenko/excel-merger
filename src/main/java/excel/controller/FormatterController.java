646b07864a435845e24c995bb8e6cbfd4de45aeb
alwxander.diachenko <mail.positiv@gmail.com>
2018-03-29 11:36:05 +0300

    Забиндил влючение филда и велью к чекбоксу

--- src/main/java/excel/controller/FormatterController.java
+++ src/main/java/excel/controller/FormatterController.java
@@ -3,20 +3,24 @@ package excel.controller;
 import excel.model.Excel;
 import excel.model.ExcelImpl;
 import excel.service.FormatterService;
+import javafx.beans.binding.BooleanBinding;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * @author Alexander Diachenko.
  */
-public class FormatterController {
+public class FormatterController implements Initializable {
 
     @FXML
     private AnchorPane anchorPane;
@@ -25,7 +29,7 @@ public class FormatterController {
     @FXML
     private CheckBox optionsCheckBox;
     @FXML
-    private TextField field;
+    private Spinner<Integer> field;
     @FXML
     private TextField value;
     @FXML
@@ -33,6 +37,11 @@ public class FormatterController {
     @FXML
     private ProgressIndicator progressIndicator;
     private List<File> files;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        init();
+    }
 
     public void selectFiles() {
         FileChooser fileChooser = new FileChooser();
@@ -46,21 +55,34 @@ public class FormatterController {
 
     public void format() {
         Excel excel = new ExcelImpl();
-        FormatterService formatterService = new FormatterService(excel, files, field.getText(), value.getText());
+        FormatterService formatterService = new FormatterService(excel, files, field.getValue(), value.getText());
         Task<Void> task = formatterService.createTask();
         progressIndicator.visibleProperty().bind(task.runningProperty());
         new Thread(task).start();
-        task.setOnSucceeded(event -> {
-            //TODO придумать вывод завершения
-            complete.setText("DONE");
-        });
-        task.setOnFailed(event -> {
-            //TODO придумать вывод ошибок
-            complete.setText(task.getException().getMessage());
-        });
+        task.setOnSucceeded(event -> setComplete());
+        task.setOnFailed(event -> setFailed(task));
+    }
+
+    private void init() {
+        field.disableProperty().bind(getBooleanBinding());
+        value.disableProperty().bind(getBooleanBinding());
     }
 
     private Stage getStage() {
         return (Stage) anchorPane.getScene().getWindow();
+    }
+
+    private void setComplete() {
+        //TODO придумать вывод завершения
+        complete.setText("DONE");
+    }
+
+    private void setFailed(Task<Void> task) {
+        //TODO придумать вывод ошибок
+        complete.setText(task.getException().getMessage());
+    }
+
+    private BooleanBinding getBooleanBinding() {
+        return optionsCheckBox.selectedProperty().not();
     }
 }
