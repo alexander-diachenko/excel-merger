9d8690392717a1b04bb1e5ba78db2eeba1dc951c
alwxander.diachenko <mail.positiv@gmail.com>
2018-04-06 13:28:24 +0300

    Добавил контроллер для меню

---
+++ src/main/java/excel/controller/MenuController.java
@@ -0,0 +1,15 @@
+package excel.controller;
+
+/**
+ * @author Alexander Diachenko.
+ */
+public class MenuController {
+
+    public void languageEnAction() {
+
+    }
+
+    public void languageRuAction() {
+
+    }
+}
